e4c090e6a954ae994bd93d0e218b656e4bbae93e
RobinK86 <150858069+RobinK86@users.noreply.github.com>
2023-11-24 13:15:44 -0600

    RE-added corrected files

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+/*  
+Author: Robin Kelley
+File Name: script.js
+Date: 11/14/2023
+*/
+
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        }
+}
